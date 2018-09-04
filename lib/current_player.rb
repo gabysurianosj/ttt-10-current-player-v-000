5c5ce5522fa8461d82c6fa31ac5daa979f075fe6
Gabriela Suriano <surianogabriela@gmail.com>
2018-09-04 13:46:30 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   turns = 0 
   board.each do |space|
-    if space == "x" || space == "o"
+    if space == "X" || space == "O"
       turns += 1 
     end
   end 
@@ -11,8 +11,8 @@ end
 def current_player(board)
   turns = turn_count(board)
   if turns % 2 == 0 
-    return "x"
+    return "X"
   else 
-    return "o"
+    return "O"
   end
 end 
